5ac24e195a3f1b8294d0e32390417865cd7ccaf9
ionutvp <ionutvp@pconsult.ro>
2023-08-14 14:03:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from sentence_transformers import SentenceTransformer
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+def read_root():
+    model = SentenceTransformer('clip-ViT-B-32')
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
